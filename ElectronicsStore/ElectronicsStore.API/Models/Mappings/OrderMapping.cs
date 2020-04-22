af3679df7876c9330d92f21efdf768bd30c0e02c
Valya <valentinaburdina1994@gmail.com>
2020-04-22 10:11:04 +0300

    Order mapping was added

---
+++ ElectronicsStore/ElectronicsStore.API/Models/Mappings/OrderMapping.cs
@@ -0,0 +1,15 @@
+﻿
+using ElecronicsStore.DB.Models.DataModels;
+using ElectronicsStore.API.Models.InputModels;
+using ElectronicsStore.API.Models.OutputModels;
+
+namespace ElectronicsStore.API.Models.Mappings
+{
+    public static class OrderMapping
+    {
+        public static Order FromInputToDataModel (OrderInputModel inputModel)
+        {
+            return new Order();
+        }
+    }
+}
